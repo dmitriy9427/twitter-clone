a017e052f8323e6ac211cd347be9a35e4ad9a013
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-12-28 20:18:33 +0300

    component auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import Auth from './pages/Auth';
+import { Routes, Route } from 'react-router-dom';
+import Auth from './pages/Auth/Auth';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Routes>
+        <Route path='/:page' element={<Auth />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
 
-
     return (
-        <>
-
-        </>
+        <div style={{ opacity: "999" }}>
+            <form>
+                <label htmlFor="email">
+                    Введите email:
+                    <input type="email" id='email' />
+                </label>
+                <label htmlFor="password">
+                    Введите пароль:
+                    <input type="password" />
+                </label>
+                <button>Войти</button>
+                <Link to={'/register'}>Зарегистрироваться</Link>
+            </form>
+        </div>
     )
 }
 
